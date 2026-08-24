8e5912b7cad68ef0d4342617993c3a6bbc2b9ff7
PolkivMIS <mis1@rstradehouse.co.in>
2026-08-24 13:36:56 +0530

    Modify forms for process_coordinator_MEGHA role
    Updated the forms for process_coordinator_MEGHA role.

--- database/roles.js
+++ database/roles.js
@@ -14,7 +14,7 @@ const roles = {
   },
   process_coordinator_MEGHA: {
     sheets: ["gpo_rs","assigned_task_rs","cashbook_megha","ims_megha","msl_counter_megha","production_counter_megha","p_operation","p_assigned_task","ne_operation","ne_assigned_task","machinary_po_tracker","machinary_po"],
-    forms: ["leave_form"]
+    forms: ["leave_form","gpo_ne_form"]
   },
   junior_accountant_RS: {
     sheets: ["gpo_bs","assigned_task_rs","store_expense"],
