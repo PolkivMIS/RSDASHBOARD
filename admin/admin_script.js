632268d4f20e3981fc5ceb77bd2cd1641ce5af0b
PolkivMIS <mis1@rstradehouse.co.in>
2026-09-23 15:37:07 +0530

    Update admin_script.js

--- admin/admin_script.js
+++ admin/admin_script.js
@@ -19,6 +19,7 @@ const mainFrame = document.getElementById("mainFrame");
 
 // Define companies (must match forms.js and sheets.js exactly)
 const companies = [
+  "COMMON",
   "B.S.GROUP",
   "R.S.TRADE",
   "NORTHEAST FNB",
