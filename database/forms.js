4a769d58a9b52841886394235c683013d09ba288
PolkivMIS <mis1@rstradehouse.co.in>
2026-09-23 15:15:46 +0530

    Change company names in forms to 'FORMS'
    Updated company names for various forms to 'FORMS'.

--- database/forms.js
+++ database/forms.js
@@ -1,119 +1,119 @@
 const forms = {
   gpo_rs_form: {
     name: "GPO (RS) Form",
-    company: "R.S.TRADE",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/1P92DXx3cx58Lcs6uj_halsJMN26Q6_5HIuA4XemxMoY/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1P92DXx3cx58Lcs6uj_halsJMN26Q6_5HIuA4XemxMoY/edit",
     icon: "../database/icons/FORM.gif"
   },
   gpo_bs_form: {
     name: "GPO (BS) Form",
-    company: "B.S.GROUP",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/1iZ8dsPOdg7fdd2wiwhR3Js2yBjxew1Q_mDxd0CurU6U/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1iZ8dsPOdg7fdd2wiwhR3Js2yBjxew1Q_mDxd0CurU6U/edit",
     icon: "../database/icons/FORM.gif"
   },
   store_repair_form: {
     name: "STORE REPAIR Form",
-    company: "B.S.GROUP",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/e/1FAIpQLSdJeF2rzgDg-eh6n0ZbZj82_iVZ0Ke9CPXGmTXQgKH5HWM9Vg/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1FAIpQLSdJeF2rzgDg-eh6n0ZbZj82_iVZ0Ke9CPXGmTXQgKH5HWM9Vg/edit",
     icon: "../database/icons/FORM.gif"
   },
   daily_sales_form: {
     name: "DAILY SALES Form (KILLER)",
-    company: "B.S.GROUP",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/1w2haEToALZjeR-UMWx1g7TarW457oDGx9PD4b2BlRCI/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1w2haEToALZjeR-UMWx1g7TarW457oDGx9PD4b2BlRCI/edit",
     icon: "../database/icons/FORM.gif"
   },
   daily_sales_form_pepe: {
     name: "DAILY SALES Form (PEPE)",
-    company: "B.S.GROUP",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/e/1FAIpQLSdZmYyvj2_O9OglMzMnV6opC5P77opOTYCyW5D3EcZPxCKf3A/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1FAIpQLSdZmYyvj2_O9OglMzMnV6opC5P77opOTYCyW5D3EcZPxCKf3A/edit",
     icon: "../database/icons/FORM.gif"
   },
   daily_sales_form_puma: {
     name: "DAILY SALES Form (PUMA)",
-    company: "B.S.GROUP",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/e/1FAIpQLSe7biNFzQhi-UlBXDpshJjjCV83vkVVDLTGpJ5l8wAQXPh55w/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1FAIpQLSdZmYyvj2_O9OglMzMnV6opC5P77opOTYCyW5D3EcZPxCKf3A/edit",
     icon: "../database/icons/FORM.gif"
   },
   store_leave_form: {
     name: "STORE LEAVE Form",
-    company: "B.S.GROUP",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/e/1FAIpQLScLsy-Cv6CvYXdWlb6wIjmeaEpkkraMVGXXDD-po5-1N9XcZA/viewform?usp=dialog",
     script: "https://docs.google.com/forms/d/1FAIpQLScLsy-Cv6CvYXdWlb6wIjmeaEpkkraMVGXXDD-po5-1N9XcZA/edit",
     icon: "../database/icons/FORM.gif"
   },
   leave_form: {
     name: "LEAVE Form",
-    company: "R.S.TRADE",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/1AGywBSLjAM4d2QXAFw3-aONU4Zq9kjbdxigxFlkEqLU/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1AGywBSLjAM4d2QXAFw3-aONU4Zq9kjbdxigxFlkEqLU/edit",
     icon: "../database/icons/FORM.gif"
   },
   advance_form: {
     name: "ADVANCE Form",
-    company: "R.S.TRADE",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/1blYDQi0Whq6j35ZzYaM5PhvR1h_kao39_EVZ3Pz6IuE/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1blYDQi0Whq6j35ZzYaM5PhvR1h_kao39_EVZ3Pz6IuE/edit",
     icon: "../database/icons/FORM.gif"
   },
   sales_form: {
     name: "SALES Form",
-    company: "B.S.GROUP",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/1q6AXHtnH3cO5a392MFFAH-CJaHXFrQU3dNKBEBF501w/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1q6AXHtnH3cO5a392MFFAH-CJaHXFrQU3dNKBEBF501w/edit",
     icon: "../database/icons/FORM.gif"
   },
   delegation_form: {
     name: "DELEGATION Form",
-    company: "R.S.TRADE",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/11F1iDlRGFLdDU5aWRltPLW_97v-ZFcKE6WK_9V7nuCg/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/11F1iDlRGFLdDU5aWRltPLW_97v-ZFcKE6WK_9V7nuCg/edit",
     icon: "../database/icons/FORM.gif"
   },
   new_party_form: {
     name: "NEW PARTY Form",
-    company: "B.S.GROUP",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/e/1FAIpQLSfDqonxv2ncL0OO_McIf9PtNh3Ht6OgMAKB4Hyg1wBVxW5UpA/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/e/1FAIpQLSfDqonxv2ncL0OO_McIf9PtNh3Ht6OgMAKB4Hyg1wBVxW5UpA/viewform?usp=dialog",
     icon: "../database/icons/FORM.gif"
   },
   ne_fnb_expense_form: {
     name: "NE FNB (EXPENSE Form)",
-    company: "NORTHEAST FNB",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/1SCBD2boj3Szn0i9sNynRwY7P5F6BDInno40kKj2ksXs/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1SCBD2boj3Szn0i9sNynRwY7P5F6BDInno40kKj2ksXs/edit",
     icon: "../database/icons/FORM.gif"
   },
   recruitment_form: {
     name: "RECRUITMENT FORM",
-    company: "OTHERS",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/e/1FAIpQLSfEQJEUwDBQaaHVDW75OGo0nMteUs-bl6Oli26PDJ55N837Tw/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1FAIpQLSfEQJEUwDBQaaHVDW75OGo0nMteUs-bl6Oli26PDJ55N837Tw/edit",
     icon: "../database/icons/HIRING.gif"
   },
   travel_expense_form: {
     name: "TRAVEL EXPENSE Form",
-    company: "B.S.GROUP",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/1CaBQ4czBjh80RobIMK5WhTrXDNzzl5QA5k_kNjR7TJk/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1CaBQ4czBjh80RobIMK5WhTrXDNzzl5QA5k_kNjR7TJk/edit",
     icon: "../database/icons/FORM.gif"
   },
   gpo_ne_form: {
     name: "GPO (NE FNB) Form",
-    company: "NORTHEAST FNB",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/e/1FAIpQLSeJcLDShNid7S-2B-r4X1Ly1SZz3VtAYZCbuE0y1MZmba8pKw/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/174eiE6n6O67skcXMnQD2Q-RzN_NQNoPb98i38hwYoV4/edit",
     icon: "../database/icons/FORM.gif"
   },
   megha_ims: {
     name: "IMS FORM",
-    company: "MEGHA AGRO",
+    company: "FORMS",
     url: "https://docs.google.com/forms/d/e/1FAIpQLSfN1DZHLZzGCv5EheKminx4p9lWkVqPSOqMZi9yyWiC1QBb8A/viewform?usp=dialog",
     script: "https://docs.google.com/forms/d/1FAIpQLSfN1DZHLZzGCv5EheKminx4p9lWkVqPSOqMZi9yyWiC1QBb8A/edit",
     icon: "../database/icons/FORM.gif"
