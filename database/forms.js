7985ce31bd257325e0eedd08be36c6feac4d41c1
PolkivMIS <mis1@rstradehouse.co.in>
2026-09-23 15:58:27 +0530

    Refactor forms object structure in forms.js

--- database/forms.js
+++ database/forms.js
@@ -1,23 +1,9 @@
 const forms = {
-  gpo_rs_form: {
-    name: "GPO (RS) Form",
+  advance_form: {
+    name: "ADVANCE Form",
     company: "FORMS",
-    url: "https://docs.google.com/forms/d/1P92DXx3cx58Lcs6uj_halsJMN26Q6_5HIuA4XemxMoY/viewform?embedded=true",
-    script: "https://docs.google.com/forms/d/1P92DXx3cx58Lcs6uj_halsJMN26Q6_5HIuA4XemxMoY/edit",
-    icon: "../database/icons/FORM.gif"
-  },
-  gpo_bs_form: {
-    name: "GPO (BS) Form",
-    company: "FORMS",
-    url: "https://docs.google.com/forms/d/1iZ8dsPOdg7fdd2wiwhR3Js2yBjxew1Q_mDxd0CurU6U/viewform?embedded=true",
-    script: "https://docs.google.com/forms/d/1iZ8dsPOdg7fdd2wiwhR3Js2yBjxew1Q_mDxd0CurU6U/edit",
-    icon: "../database/icons/FORM.gif"
-  },
-  store_repair_form: {
-    name: "STORE REPAIR Form",
-    company: "FORMS",
-    url: "https://docs.google.com/forms/d/e/1FAIpQLSdJeF2rzgDg-eh6n0ZbZj82_iVZ0Ke9CPXGmTXQgKH5HWM9Vg/viewform?embedded=true",
-    script: "https://docs.google.com/forms/d/1FAIpQLSdJeF2rzgDg-eh6n0ZbZj82_iVZ0Ke9CPXGmTXQgKH5HWM9Vg/edit",
+    url: "https://docs.google.com/forms/d/1blYDQi0Whq6j35ZzYaM5PhvR1h_kao39_EVZ3Pz6IuE/viewform?embedded=true",
+    script: "https://docs.google.com/forms/d/1blYDQi0Whq6j35ZzYaM5PhvR1h_kao39_EVZ3Pz6IuE/edit",
     icon: "../database/icons/FORM.gif"
   },
   daily_sales_form: {
@@ -41,11 +27,32 @@ const forms = {
     script: "https://docs.google.com/forms/d/1FAIpQLSdZmYyvj2_O9OglMzMnV6opC5P77opOTYCyW5D3EcZPxCKf3A/edit",
     icon: "../database/icons/FORM.gif"
   },
-  store_leave_form: {
-    name: "STORE LEAVE Form",
+  delegation_form: {
+    name: "DELEGATION Form",
     company: "FORMS",
-    url: "https://docs.google.com/forms/d/e/1FAIpQLScLsy-Cv6CvYXdWlb6wIjmeaEpkkraMVGXXDD-po5-1N9XcZA/viewform?usp=dialog",
-    script: "https://docs.google.com/forms/d/1FAIpQLScLsy-Cv6CvYXdWlb6wIjmeaEpkkraMVGXXDD-po5-1N9XcZA/edit",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSe1yK0cZ5g727gQ7qnoHDu3ZxzaPaZC9o6dG-JA5WT8boCL8w/viewform?usp=dialog",
+    script: "https://docs.google.com/forms/d/11F1iDlRGFLdDU5aWRltPLW_97v-ZFcKE6WK_9V7nuCg/edit",
+    icon: "../database/icons/FORM.gif"
+  },
+  gpo_bs_form: {
+    name: "GPO (BS) Form",
+    company: "FORMS",
+    url: "https://docs.google.com/forms/d/1iZ8dsPOdg7fdd2wiwhR3Js2yBjxew1Q_mDxd0CurU6U/viewform?embedded=true",
+    script: "https://docs.google.com/forms/d/1iZ8dsPOdg7fdd2wiwhR3Js2yBjxew1Q_mDxd0CurU6U/edit",
+    icon: "../database/icons/FORM.gif"
+  },
+  gpo_rs_form: {
+    name: "GPO (RS) Form",
+    company: "FORMS",
+    url: "https://docs.google.com/forms/d/1P92DXx3cx58Lcs6uj_halsJMN26Q6_5HIuA4XemxMoY/viewform?embedded=true",
+    script: "https://docs.google.com/forms/d/1P92DXx3cx58Lcs6uj_halsJMN26Q6_5HIuA4XemxMoY/edit",
+    icon: "../database/icons/FORM.gif"
+  },
+  gpo_ne_form: {
+    name: "GPO (NE FNB) Form",
+    company: "FORMS",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSeJcLDShNid7S-2B-r4X1Ly1SZz3VtAYZCbuE0y1MZmba8pKw/viewform?embedded=true",
+    script: "https://docs.google.com/forms/d/174eiE6n6O67skcXMnQD2Q-RzN_NQNoPb98i38hwYoV4/edit",
     icon: "../database/icons/FORM.gif"
   },
   leave_form: {
@@ -55,11 +62,11 @@ const forms = {
     script: "https://docs.google.com/forms/d/1AGywBSLjAM4d2QXAFw3-aONU4Zq9kjbdxigxFlkEqLU/edit",
     icon: "../database/icons/FORM.gif"
   },
-  advance_form: {
-    name: "ADVANCE Form",
+  new_party_form: {
+    name: "NEW PARTY Form",
     company: "FORMS",
-    url: "https://docs.google.com/forms/d/1blYDQi0Whq6j35ZzYaM5PhvR1h_kao39_EVZ3Pz6IuE/viewform?embedded=true",
-    script: "https://docs.google.com/forms/d/1blYDQi0Whq6j35ZzYaM5PhvR1h_kao39_EVZ3Pz6IuE/edit",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSfDqonxv2ncL0OO_McIf9PtNh3Ht6OgMAKB4Hyg1wBVxW5UpA/viewform?embedded=true",
+    script: "https://docs.google.com/forms/d/e/1FAIpQLSfDqonxv2ncL0OO_McIf9PtNh3Ht6OgMAKB4Hyg1wBVxW5UpA/viewform?usp=dialog",
     icon: "../database/icons/FORM.gif"
   },
   sales_form: {
@@ -69,18 +76,18 @@ const forms = {
     script: "https://docs.google.com/forms/d/1q6AXHtnH3cO5a392MFFAH-CJaHXFrQU3dNKBEBF501w/edit",
     icon: "../database/icons/FORM.gif"
   },
-  delegation_form: {
-    name: "DELEGATION Form",
+  store_repair_form: {
+    name: "STORE REPAIR Form",
     company: "FORMS",
-    url: "https://docs.google.com/forms/d/e/1FAIpQLSe1yK0cZ5g727gQ7qnoHDu3ZxzaPaZC9o6dG-JA5WT8boCL8w/viewform?usp=dialog",
-    script: "https://docs.google.com/forms/d/11F1iDlRGFLdDU5aWRltPLW_97v-ZFcKE6WK_9V7nuCg/edit",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSdJeF2rzgDg-eh6n0ZbZj82_iVZ0Ke9CPXGmTXQgKH5HWM9Vg/viewform?embedded=true",
+    script: "https://docs.google.com/forms/d/1FAIpQLSdJeF2rzgDg-eh6n0ZbZj82_iVZ0Ke9CPXGmTXQgKH5HWM9Vg/edit",
     icon: "../database/icons/FORM.gif"
   },
-  new_party_form: {
-    name: "NEW PARTY Form",
+  store_leave_form: {
+    name: "STORE LEAVE Form",
     company: "FORMS",
-    url: "https://docs.google.com/forms/d/e/1FAIpQLSfDqonxv2ncL0OO_McIf9PtNh3Ht6OgMAKB4Hyg1wBVxW5UpA/viewform?embedded=true",
-    script: "https://docs.google.com/forms/d/e/1FAIpQLSfDqonxv2ncL0OO_McIf9PtNh3Ht6OgMAKB4Hyg1wBVxW5UpA/viewform?usp=dialog",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScLsy-Cv6CvYXdWlb6wIjmeaEpkkraMVGXXDD-po5-1N9XcZA/viewform?usp=dialog",
+    script: "https://docs.google.com/forms/d/1FAIpQLScLsy-Cv6CvYXdWlb6wIjmeaEpkkraMVGXXDD-po5-1N9XcZA/edit",
     icon: "../database/icons/FORM.gif"
   },
   ne_fnb_expense_form: {
@@ -102,13 +109,6 @@ const forms = {
     company: "FORMS",
     url: "https://docs.google.com/forms/d/1CaBQ4czBjh80RobIMK5WhTrXDNzzl5QA5k_kNjR7TJk/viewform?embedded=true",
     script: "https://docs.google.com/forms/d/1CaBQ4czBjh80RobIMK5WhTrXDNzzl5QA5k_kNjR7TJk/edit",
-    icon: "../database/icons/FORM.gif"
-  },
-  gpo_ne_form: {
-    name: "GPO (NE FNB) Form",
-    company: "FORMS",
-    url: "https://docs.google.com/forms/d/e/1FAIpQLSeJcLDShNid7S-2B-r4X1Ly1SZz3VtAYZCbuE0y1MZmba8pKw/viewform?embedded=true",
-    script: "https://docs.google.com/forms/d/174eiE6n6O67skcXMnQD2Q-RzN_NQNoPb98i38hwYoV4/edit",
     icon: "../database/icons/FORM.gif"
   },
   megha_ims: {
