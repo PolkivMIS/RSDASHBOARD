ee490917c437fd449f421be290255633fad83a8b
PolkivMIS <mis1@rstradehouse.co.in>
2026-09-23 15:38:34 +0530

    Refactor sheets data structure in sheets.js

--- database/sheets.js
+++ database/sheets.js
@@ -247,19 +247,19 @@ const sheets = {
   },
   placeholder2: {
     name: "PLACEHOLDER",
-    company: "PENTAGON",
+    company: "COMMON",
     url: "https://docs.google.com/spreadsheets/d/15Fnuj4IGn9fsI5ecQ_varBFLxIlvC_5Opbtnux2p06Q1/edit?gid=378801912#gid=378801912",
     icon: "../database/icons/ASSIGN_TASK.gif"
   },
   placeholder3: {
     name: "PLACEHOLDER",
-    company: "PENTAGON",
+    company: "COMMON",
     url: "https://docs.google.com/spreadsheets/d/15Fnuj4IGn9fsI5ecQ_varBFLxIlvC_5Opbtnux2p06Q1/edit?gid=378801912#gid=378801912",
     icon: "../database/icons/ASSIGN_TASK.gif"
   },
   placeholder4: {
     name: "PLACEHOLDER",
-    company: "PENTAGON",
+    company: "COMMON",
     url: "https://docs.google.com/spreadsheets/d/15Fnuj4IGn9fsI5ecQ_varBFLxIlvC_5Opbtnux2p06Q1/edit?gid=378801912#gid=378801912",
     icon: "../database/icons/ASSIGN_TASK.gif"
   }
