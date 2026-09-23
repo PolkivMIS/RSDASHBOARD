66c956d6a7ec8fa60c8dabb74843e6f6b0a35e26
PolkivMIS <mis1@rstradehouse.co.in>
2026-09-23 15:36:15 +0530

    Refactor roles.js to include delegation in roles

--- database/roles.js
+++ database/roles.js
@@ -5,67 +5,77 @@ const roles = {
     forms: Object.keys(forms)
   },
   executive_assistant: {
-    sheets: ["cn_fms_rs","store_repair","delegation","assigned_task_rs","travel_expense","leave_advance","ne_fnb_expense","recruitment_fms","document_database","employee_data_bs","all_payment_db","machinary_po_tracker","panditji_tracker"],
+    sheets: ["cn_fms_rs","store_repair","delegation","assigned_task_rs","travel_expense",
+             "leave_advance","ne_fnb_expense","recruitment_fms","document_database","employee_data_bs",
+             "all_payment_db","machinary_po_tracker","panditji_tracker","delegation"],
     forms: ["leave_form","delegation_form","ne_fnb_expense_form","recruitment_form"]
   },
   process_coordinator_RS: {
-    sheets: ["gpo_rs","gpo_bs","store_repair","cn_fms_rs","collection_data","sales_master","travel_expense","shop_in_shop","daily_sales","store_attendance","billwise","checklist_rs","assigned_task_rs"],
+    sheets: ["gpo_rs","gpo_bs","store_repair","cn_fms_rs","collection_data","sales_master",
+             "travel_expense","shop_in_shop","daily_sales","store_attendance","billwise",
+             "checklist_rs","assigned_task_rs","delegation"],
     forms: ["leave_form"]
   },
   process_coordinator_MEGHA: {
-    sheets: ["gpo_ne","assigned_task_rs","cashbook_megha","ims_megha","msl_counter_megha","production_counter_megha","p_operation","p_assigned_task","ne_operation","ne_assigned_task","machinary_po_tracker","machinary_po","plant_n_machinary","ne_ims"],
+    sheets: ["gpo_ne","assigned_task_rs","cashbook_megha","ims_megha","msl_counter_megha",
+             "production_counter_megha","p_operation","p_assigned_task","ne_operation","ne_assigned_task",
+             "machinary_po_tracker","machinary_po","plant_n_machinary","ne_ims","delegation"],
     forms: ["leave_form","gpo_ne_form","ne_fnb_expense_form"]
   },
   junior_accountant_RS: {
-    sheets: ["gpo_bs","assigned_task_rs","store_expense"],
+    sheets: ["gpo_bs","assigned_task_rs","store_expense","delegation"],
     forms: ["leave_form"]
   },
   senior_accountant: {
-    sheets: ["gpo_bs","cn_fms_rs","travel_expense","assigned_task_rs"],
+    sheets: ["gpo_bs","cn_fms_rs","travel_expense","assigned_task_rs","delegation"],
     forms: ["leave_form"]
   },
   ne_junior_accountant: {
-    sheets: ["gpo_ne","ne_assigned_task"],
+    sheets: ["gpo_ne","ne_assigned_task","delegation"],
     forms: ["leave_form"]
   },
   mis: {
-    sheets: ["gpo_rs","collection_data","sales_master","travel_expense","assigned_task_rs","cn_fms_rs","leave_advance","recruitment_fms"],
+    sheets: ["gpo_rs","collection_data","sales_master","travel_expense","assigned_task_rs","cn_fms_rs",
+             "leave_advance","recruitment_fms","delegation"],
     forms: ["leave_form","advance_form"]
   },
   operation_2: {
-    sheets: ["gpo_rs","cn_fms_rs","shop_in_shop","assigned_task_rs"],
+    sheets: ["gpo_rs","cn_fms_rs","shop_in_shop","assigned_task_rs","delegation"],
     forms: ["leave_form"]
   },
   operation_1: {
-    sheets: ["gpo_bs","store_repair","daily_sales","collection_data","assigned_task_rs","leave_advance"],
+    sheets: ["gpo_bs","store_repair","daily_sales","collection_data","assigned_task_rs","leave_advance",
+             "delegation"],
     forms: ["store_repair_form","daily_sales_form","travel_expense_form","leave_form"]
   },
   operation_3: {
-    sheets: ["gpo_rs","shop_in_shop","assigned_task_rs"],
+    sheets: ["gpo_rs","shop_in_shop","assigned_task_rs","delegation"],
     forms: ["leave_form"]
   },
   operation_4: {
-    sheets: ["gpo_bs","store_repair","daily_sales_pepe","daily_sales_puma","assigned_task_rs"],
+    sheets: ["gpo_bs","store_repair","daily_sales_pepe","daily_sales_puma","assigned_task_rs","delegation"],
     forms: ["store_repair_form","daily_sales_form_pepe","daily_sales_form_puma","travel_expense_form","leave_form"]
   },
   junior_accountant_BS: {
-    sheets: ["gpo_rs","assigned_task_rs","billwise"],
+    sheets: ["gpo_rs","assigned_task_rs","billwise","delegation"],
     forms: ["leave_form"]
   },
   billing: {
-    sheets: ["gpo_rs","assigned_task_rs"],
+    sheets: ["gpo_rs","assigned_task_rs","delegation"],
     forms: ["leave_form"]
   },
   manager: {
-    sheets: ["gpo_rs","cn_fms_rs","collection_data","store_repair","sales_master","travel_expense","shop_in_shop","daily_sales","billwise","leave_advance"],
+    sheets: ["gpo_rs","cn_fms_rs","collection_data","store_repair","sales_master","travel_expense","shop_in_shop",
+             "daily_sales","billwise","leave_advance","delegation"],
     forms: ["recruitment_form"]
   },
   sales_manager: {
-    sheets: ["collection_data","store_repair","sales_master","shop_in_shop","daily_sales","billwise","leave_advance"],
+    sheets: ["collection_data","store_repair","sales_master","shop_in_shop","daily_sales","billwise","leave_advance",
+             "delegation"],
     forms: ["leave_form","recruitment_form"]
   },
   store_manager: {
-    sheets: ["assigned_task_rs","store_repair"],
+    sheets: ["assigned_task_rs","store_repair","delegation"],
     forms: ["store_leave_form","store_repair_form"]
   },
   pent_acc: {
